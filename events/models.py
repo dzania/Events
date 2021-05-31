db754d5257a669f5dd0584a6d2a792db2e299efa
dzania <artur.kantorczyk@gmail.com>
2021-05-31 22:51:26 +0200

    Created event model

--- events/models.py
+++ events/models.py
@@ -3,8 +3,15 @@ from django.utils import timezone
 
 # Create your models here.
 class Event(models.Model):
-    image = models.ImageField()
-    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='upload',null=True,blank=True)
+    organizer = models.CharField(max_length=50)
+    title = models.CharField(max_length=80)
     description = models.TextField()
-    date = models.DateTimeField()
+    start_date = models.DateTimeField(blank=False)
+    end_date = models.DateField(blank=True)
+    location = models.CharField(max_length=100)
+    price = models.IntegerField(blank=True)
     published = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
